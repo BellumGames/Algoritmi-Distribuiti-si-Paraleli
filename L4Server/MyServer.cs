a7f42ea1e74c9eb575d94c9224c109e485553493
BELLUM_GAMES <79925845+BellumGames@users.noreply.github.com>
2023-03-07 10:53:05 +0200

    Ceva la server dar incomplet

--- L4Server/MyServer.cs
+++ L4Server/MyServer.cs
@@ -7,35 +7,48 @@ using System.Threading.Tasks;
 
 namespace L4Server
 {
-    class MyServer
+    public class MyServer
     {
-        TcpListener server = null;
-        NetworkStream stream = null;
-        StreamReader streamReader = null;
-        StreamWriter streamWriter = null;
+        //Deci ca sa nu uit:
+        //Serverul prin Recive Message primeste mesaje de la clienti, in momentul primirii mesajelor de la clienti (clientii pot trimite doar mesaje gen salut) serverul trebuie sa raspund aprin send message cu count ca si index al clientului de asemenea redirectioneaza mesajul primit de la client la fiecare client conectat
+        public TcpListener server = null;
+        public NetworkStream stream = null;
+        public StreamReader streamReader = null;
+        public StreamWriter streamWriter = null;
+        public static Dictionary<string, string> log = new Dictionary<string, string>();
+        public static int count = 0;
+        public static int index = 0;
+
         public MyServer(TcpClient clientSocket)
         {
+            count++;
+            Console.WriteLine($"Client number #{index} connected");
+            log.Add($"Client {index}", "");
+            index++;
             stream = clientSocket.GetStream();
             streamReader = new StreamReader(stream);
             streamWriter = new StreamWriter(stream);
-            receiveInt();
+            ReciveMessage();
             stream.Close();
         }
+
         public MyServer()
         {
             Thread thread = new Thread(new ThreadStart(run));
             thread.Start();
         }
-        public void sendInt(string val)
+
+        public void SendMessage(string message)
         {
-            streamWriter.WriteLine(Convert.ToString(val));
             streamWriter.Flush();
         }
-        public void receiveInt()
+
+        public void ReciveMessage()
         {
-            string val = streamReader.ReadLine();
-            Console.WriteLine(val);
+
+            SendMessage(streamReader.ReadLine().ToString()); //mesaj primit de la client e redirectionat la restu
         }
+
         void run()
         {
             server = new TcpListener(8000);
@@ -47,5 +60,4 @@ namespace L4Server
             }
         }
     }
-
 }
