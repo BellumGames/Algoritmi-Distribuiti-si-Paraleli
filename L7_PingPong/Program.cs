09e8c06b0c8d8dacca7b4187947448141154c63b
BELLUM_GAMES <79925845+BellumGames@users.noreply.github.com>
2023-04-11 09:28:30 +0300

    l7 Ping Pong

---
+++ L7_PingPong/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MPI;
+
+namespace L7_PingPong
+{
+    class PingPong
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            //nr task pt fiecare proces
+            int d = 0;
+            using (new MPI.Environment(ref args))
+            {
+                Intracommunicator comm = Communicator.world;
+                d = comm.Size / array.Length;
+                if (comm.Rank == 0)
+                {
+                    Console.WriteLine("Rank 0 is alive and running on " + MPI.Environment.ProcessorName);
+                    for (int i = 0; i < d; d++)
+                    {
+                        sum += array[i];
+                    }
+                    for (int dest = 1; dest < comm.Size; ++dest) 
+                    {
+                        sum += comm.Receive<int>(dest, 0);
+                    }
+                    Console.WriteLine($"Suma este = {sum}");
+                }
+                else
+                {
+                    //lim inf pt suma
+                    int limInf = comm.Rank * d;
+                    //lim sup pt suma
+                    int limSum = limInf * d;
+                    if (comm.Rank == comm.Size - 1) 
+                    {
+                        for (int i = limInf; i < limSum; i++) 
+                        {
+                            sum += array[i];
+                        }
+                        comm.Send<int>(sum, 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
